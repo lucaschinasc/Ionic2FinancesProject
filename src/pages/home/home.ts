d0d998012979b4d8e5428dda7008ee88d598bf33
lucaschinasc <lucaschioqueti@gmail.com>
2017-03-01 23:40:35 -0300

    adicionado o timer...

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,19 +12,31 @@ export class HomePage {
 
   public nome;
 
+public timeLeft: number = 100;
+
   constructor(public navCtrl: NavController) {
     
     this.nome = "Página principal";
 
+
+var timer = setInterval(() => {
+  if(this.timeLeft != 0) {
+    this.timeLeft -=  10;
+    
+    this.randomize( Math.floor((Math.random() * 100) + 1) );    
+  } else {
+    clearInterval(timer);
+  }
+ }, 2700);
 
   }
 
 
    // lineChart
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [10, 15, 20, 10, 30, 20, 10], label: 'Series A'},
+    {data: [10, 50, 60, 30, 50, 40, 60], label: 'Series B'},
+    {data: [18, 48, 77, 9, 40, 27, 40], label: 'Series C'}
   ];
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions:any = {
@@ -59,7 +71,7 @@ export class HomePage {
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
  
-  public randomize():void {
+  public randomize( valor ):void {
     let _lineChartData:Array<any> = new Array(this.lineChartData.length);
     for (let i = 0; i < this.lineChartData.length; i++) {
       _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
